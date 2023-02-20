863aa644565dd1f8b20d3fde4df5776c0ec6148d
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-02-20 02:08:41 +0200

    fix task #4

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import user from "../data/user.json";
 import data from "../data/data.json";
-import friends from "../data/friends.json"
+import friends from "../data/friends.json";
+import transactions from "../data/transactions.json";
+
 import { Profile } from "./profile/Profile";
 import { Statistics } from "./statistics/Statistics";
 import { FriendList } from "./friendList/FriendList";
-
+import { TransactionHistory } from "./transaction/TransactionHistory";
 
 export const App = () => {
   return (
@@ -26,7 +28,8 @@ export const App = () => {
         stats={user.stats} />
       
       <Statistics title="Upload stats" stats={data}/>
-      <FriendList friends={friends}/>;
+      <FriendList friends={friends} />;
+      <TransactionHistory items={transactions} />;
     
     </div>
     
