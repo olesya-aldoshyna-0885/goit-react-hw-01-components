aca46257feabe2dbd0ff811747b9657853bc6382
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-03-02 20:44:11 +0200

    .

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import user from "../Profile/user.json";
+import user from "./user.json";
 import data from "../Statistics/data.json";
 import friends from "../FriendList/friends.json";
 import transactions from "../Transaction/transactions.json";
