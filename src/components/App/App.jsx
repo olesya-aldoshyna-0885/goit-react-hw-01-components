be84abdb6e300be0feb17de90cd46bba7b8c4d0e
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-03-03 16:10:45 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,10 +4,10 @@ import { FriendList } from '../friendList/FriendList';
 import { TransactionHistory } from '../transaction/TransactionHistory';
 import { Container } from './App.styled';
 
-import user from '../data/user.json';
-import data from '../data/data.json';
-import friends from '../data/friends.json';
-import transactions from '../data/transactions.json';
+import user from 'data/user.json';
+import data from 'data/data.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
 
 export const App = () => {
   return (
