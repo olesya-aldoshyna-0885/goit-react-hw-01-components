2d09daf46f699f4ce1de0de72079a047c0a44141
Olesya Aldoshyna <olesyaaldoshyna@gmail.com>
2023-03-03 15:44:19 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,13 @@
-import user from "../Data/user.json";
-import data from "../Data/data.json";
-import friends from "../Data/friends.json";
-import transactions from "../Data/transactions.json";
+import user from '../Data/user.json';
+import data from '../Data/data.json';
+import friends from '../Data/friends.json';
+import transactions from '../Data/transactions.json';
 
-import { Profile } from "../Profile/Profile";
-import { Statistics } from "../Statistics/Statistics";
-import { FriendList } from "../FriendList/FriendList";
-import { TransactionHistory } from "../Transaction/TransactionHistory";
-import { Container } from "./App.styled"
+import { Profile } from '../Profile/Profile';
+import { Statistics } from '../Statistics/Statistics';
+import { FriendList } from '../FriendList/FriendList';
+import { TransactionHistory } from '../Transaction/TransactionHistory';
+import { Container } from './App.styled'
 
 export const App = () => {
   return (
